7e60a6a6a894694b545e8e88ef4f90cefb9b661a
3115988782@qq.com <3115988782@qq.com>
2022-07-04 21:21:05 +0800

    修改编辑页面接口Url

--- src/service/write.js
+++ src/service/write.js
@@ -1,49 +1,44 @@
-import { stringify } from 'qs'
-import request from '@/utils/request'
+import { stringify } from 'qs';
+import request from '@/utils/request';
 
 // 保存draft
 export async function createDraft(data) {
-  return request('/api/create/draft', {
+  return request('/api/write/saveDraft', {
     method: 'POST',
     data,
-  })
+  });
 }
 
 // 获取 draft
 export async function getDraft(params) {
-  return request(`/api/draft?${stringify(params)}`)
+  return request(`/api/write/draft?${stringify(params)}`);
 }
 
 // 获取drafts
 export async function getDrafts() {
-  return request('/api/drafts')
+  return request('/api/write/drafts');
 }
 
 // 更新draft
 export async function updateDraft(data) {
-  return request('/api/update/draft', {
+  return request('/api/write/updateDraft', {
     method: 'POST',
     data,
-  })
+  });
 }
 
 // 删除draft
 export async function deleteDraft(data) {
-  return request('/api/delete/draft', {
+  return request('/api/write/deleteDraft', {
     method: 'POST',
     data,
-  })
-}
-
-// 获到标签和分类
-export async function getCategories() {
-  return request('/api/categories')
+  });
 }
 
 // 发布文章
 export async function createPublish(data) {
-  return request('/api/create/publish', {
+  return request('/api/write/publish', {
     method: 'POST',
     data,
-  })
+  });
 }
